bd7929530506bab97a3ed48a1b6b5dca017f1303
Pedro Henrique Ramos <pedrohramos.ti@gmail.com>
2024-07-05 22:54:00 -0300

    feat: make a stuppid consumer

---
+++ Kafka.Consumer.Console/ParametersModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kafka.Consumer.Console
+{
+    public class ParametersModel
+    {
+
+        public ParametersModel() {
+            BootstrapServer = "localhost:9092";
+            TopicName = "pramos-topic";
+            GroupId = "Group 1";
+        }
+
+        public string BootstrapServer { get; set; }
+        public string TopicName { get; set; }
+        public string GroupId { get; set; }
+    }
+}
---
+++ Kafka.Consumer.Console/Program.cs
@@ -0,0 +1,9 @@
+﻿using Kafka.Consumer.Console.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+IHost host = Host.CreateDefaultBuilder(args).ConfigureServices(services =>{
+        services.AddHostedService<ConsumerService>();
+    }).Build();
+
+await host.RunAsync();
