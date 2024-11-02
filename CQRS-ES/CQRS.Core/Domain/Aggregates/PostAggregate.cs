d7cd5f4d3138bf730683f0f44255cecbdf081f45
Pedro Henrique Ramos <pedrohramos.ti@gmail.com>
2024-11-02 20:04:52 -0300

    fix: project references

---
+++ CQRS-ES/CQRS.Core/Domain/Aggregates/PostAggregate.cs
@@ -0,0 +1,31 @@
+﻿using Post.Common.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS.Core.Domain.Aggregates
+{
+    public class PostAggregate : AggregateRoot
+    {
+        private bool _isActive;
+        private string _author;
+        private readonly Dictionary<Guid, Tuple<string, string>> _comments = new Dictionary<Guid, Tuple<string, string>>();
+
+        public bool IsActive
+        {
+            get => _isActive; set => _isActive = value;
+        }
+
+        public PostAggregate() { }
+
+        /*public PostAggregate(Guid id, string author, string message) {
+            RaiseEvent(new PostCreatedEvent
+            {
+
+            });
+        }*/
+
+    }
+}
