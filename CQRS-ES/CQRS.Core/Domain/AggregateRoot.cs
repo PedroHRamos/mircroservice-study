89042786d5a1ca9e4549cac5b0c2138f058a9fbf
Pedro Henrique Ramos <pedrohramos.ti@gmail.com>
2024-11-02 17:54:33 -0300

    feat: add AggregateRoot

---
+++ CQRS-ES/CQRS.Core/Domain/AggregateRoot.cs
@@ -0,0 +1,55 @@
+﻿using CQRS.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS.Core.Domain
+{
+    public abstract class AggregateRoot
+    {
+        protected Guid _id;
+        private IList<BaseEvent> _changes = new List<BaseEvent>();
+
+        public Guid Id { get { return _id; } }
+        public int Version { get; set; } = -1;
+
+        public IEnumerable<BaseEvent> GetUncommittedChanges()
+        {
+            return _changes;
+        }
+
+        public void MarkChangeAsCommited()
+        {
+            _changes.Clear();
+        }
+
+        private void ApplyChange(BaseEvent @event, bool isNew)
+        {
+            var method = this.GetType().GetMethod("Apply", new Type[] { @event.GetType() });
+
+            if(method is null)
+                throw new ArgumentNullException(nameof(method), $" The apply method was not found in the aggregate for {@event.GetType().Name}");
+
+            method.Invoke(this, new object[] { @event });
+
+            if(isNew)
+                _changes.Add(@event);
+        }
+
+        protected void RaiseEvent(BaseEvent @event) { 
+            
+            ApplyChange(@event, true);
+            
+        }
+
+        public void ReplayEvents(IEnumerable<BaseEvent> events)
+        {
+            foreach (var @event in events)
+            {
+                ApplyChange(@event, false);
+            }
+        }
+    }
+}
